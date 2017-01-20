ed95d9be524d0beee1371a93f9d505ec2d4d2d92
KURISUTIAN <KURISUTIAN@Internet>
2017-01-19 21:45:40 -0600

    cree las clases de conexionConTablaPagos y ConexionConRecibos para separar todo muy bien, y elimine los metodos existentes en conexionSQL para q estuvieran donde debian estar

--- CemprodecaDataManager/src/conexionesBD/ConexionConRecibos.java
+++ CemprodecaDataManager/src/conexionesBD/ConexionConRecibos.java
@@ -1,5 +1,22 @@
 package conexionesBD;
 
+import java.util.ArrayList;
+
+import estructuraDeDatos.Recibos;
+
 public class ConexionConRecibos {
 
+	
+	public ArrayList<Recibos> DescargaRecibos(){
+		System.out.println("NO ESTA DEFINIDO EL CONTENIDO DEL METODO");
+	return null;/*retorna los recibos viejos de la db*/
+	}/*fin de Recibos entrantes*/
+	
+
+	
+	public void CargarRecibosAlaDB(){
+		
+	}/*fin de carga de recibos*/
+	
+	
 }
