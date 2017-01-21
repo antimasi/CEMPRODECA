e0980f158c505349736ecb322e955d9e61f2ae52
KURISUTIAN <KURISUTIAN@Internet>
2017-01-21 15:51:13 -0600

    agregacion de las clases buscarPersonaPorCedula y IngresarPersonaAlSistema

--- CemprodecaDataManager/src/LogicaDeNegocio/LogicaDeNegocioDePersona.java
+++ CemprodecaDataManager/src/LogicaDeNegocio/LogicaDeNegocioDePersona.java
@@ -1,21 +1,33 @@
 package LogicaDeNegocio;
 
+import conexionesBD.ConexionConPersonas;
 import estructuraDeDatos.Persona;
 
-public class LogicaDeNegocioDePersona {
+public class LogicaDeNegocioDePersona 
+
+    {
 
 	Persona LaPersona;
-	
-	public Persona BuscarPersonaPorCedula(Persona LaPersona)
+	ConexionConPersonas GestorDePersonas;
+	public Persona BuscarPersonaPorCedula(String Cedula)
 	{
 	Persona laPersonaBuscada;
-	
-	
-	
-	
+	GestorDePersonas=new ConexionConPersonas();
+	laPersonaBuscada=GestorDePersonas.ObtenerPersonaDeLaBaseDeDatos(Cedula);
+		
 	return laPersonaBuscada;	
 		
 	}
+	
+	public void IngresarPersonaAlSistema(Persona LaNuevaPersona)
+	{
+		GestorDePersonas=new ConexionConPersonas();
+		GestorDePersonas.IngresarPersona(LaNuevaPersona);
+		
+		
+	}
+	
+	
 	
 	
 	
