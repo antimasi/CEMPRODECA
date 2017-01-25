a9d6c7fedb37661b48a5da922eedacde7496e2ac
Anubarakdesu <Anubarakdewaarimasen@gmail.com>
2017-01-25 16:01:30 -0600

    Modificaciones minimas

--- CemprodecaDataManager/src/impresion/ImpresionDeRecibos.java
+++ CemprodecaDataManager/src/impresion/ImpresionDeRecibos.java
@@ -1,10 +1,23 @@
 package impresion;
 
-public class ImpresionDeRecibos {
+import java.awt.Graphics;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import java.awt.print.PrinterException;
 
-	public boolean Entrada(String imprimeEsto){
+import estructuraDeDatos.Operaciones;
+import estructuraDeDatos.Persona;
+
+public class ImpresionDeRecibos implements Printable {
+
+	public boolean EntradaRecibos(Persona cliente){
 		
 		return true;/*cambia si se ha podido encolar la impresion o no*/
+	}
+
+	@Override
+	public int print(Graphics grafico, PageFormat formato, int indiceDePagina) throws PrinterException {
+		return 0;
 	}
 	
 }/*fin de clase*/
