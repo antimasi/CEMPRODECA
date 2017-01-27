6fec7b4684aff6402b1713fb525478e6802e8282
KURISUTIAN <KURISUTIAN@Internet>
2017-01-27 16:13:20 -0600

    le agregue la variable cedula, para que el objeto tenga la cedula correspondiente con el numero de operacion.  todavia me cuesta entender la logica por eso lo agregue

--- CemprodecaDataManager/src/estructuraDeDatos/Operaciones.java
+++ CemprodecaDataManager/src/estructuraDeDatos/Operaciones.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 public class Operaciones {
 	
+	String Cedula;
 	String numeroDeOperacion;
 	Date fechaDeFormalizacion;
 	Date fechaDeCancelacion;
@@ -38,6 +39,96 @@ public class Operaciones {
 	public void setRecibos(ArrayList<Recibos> r){
 		this.recibos = r;
 	}/*fin de llenado de recibos*/
+
+
+	public String getCedula() {
+		return Cedula;
+	}
+
+
+	public void setCedula(String cedula) {
+		Cedula = cedula;
+	}
+
+
+	public String getNumeroDeOperacion() {
+		return numeroDeOperacion;
+	}
+
+
+	public void setNumeroDeOperacion(String numeroDeOperacion) {
+		this.numeroDeOperacion = numeroDeOperacion;
+	}
+
+
+	public Date getFechaDeFormalizacion() {
+		return fechaDeFormalizacion;
+	}
+
+
+	public void setFechaDeFormalizacion(Date fechaDeFormalizacion) {
+		this.fechaDeFormalizacion = fechaDeFormalizacion;
+	}
+
+
+	public Date getFechaDeCancelacion() {
+		return fechaDeCancelacion;
+	}
+
+
+	public void setFechaDeCancelacion(Date fechaDeCancelacion) {
+		this.fechaDeCancelacion = fechaDeCancelacion;
+	}
+
+
+	public double getMontoDelPrestmo() {
+		return montoDelPrestmo;
+	}
+
+
+	public void setMontoDelPrestmo(double montoDelPrestmo) {
+		this.montoDelPrestmo = montoDelPrestmo;
+	}
+
+
+	public double getTaza() {
+		return taza;
+	}
+
+
+	public void setTaza(double taza) {
+		this.taza = taza;
+	}
+
+
+	public double getPagoMensual() {
+		return pagoMensual;
+	}
+
+
+	public void setPagoMensual(double pagoMensual) {
+		this.pagoMensual = pagoMensual;
+	}
+
+
+	public boolean isActivo() {
+		return activo;
+	}
+
+
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+
+
+	public ArrayList<TablaDePagos> getTablaDePagos() {
+		return tablaDePagos;
+	}
+
+
+	public ArrayList<Recibos> getRecibos() {
+		return recibos;
+	}
 	
 	
 }/*fin de clase*/
