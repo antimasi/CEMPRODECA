a36d8e72054ffaa6792d356aadd773c1022fdf00
KURISUTIAN <KURISUTIAN@Internet>
2017-02-01 23:10:37 -0600

    creacion de metodo ObtenerLaListaDeOperacionesDeLaPersona, en el cual se obtienen las operaciones que tienen un monto diferente de cero, ahi hay que revisar eso

--- CemprodecaDataManager/src/LogicaDeNegocio/LogicaDeNegocioDeOperaciones.java
+++ CemprodecaDataManager/src/LogicaDeNegocio/LogicaDeNegocioDeOperaciones.java
@@ -1,5 +1,19 @@
 package LogicaDeNegocio;
 
+import java.awt.List;
+
+import conexionesBD.ConexionConOperaciones;
+import estructuraDeDatos.Operaciones;
 public class LogicaDeNegocioDeOperaciones {
+	
+	ConexionConOperaciones GestorDeOperaciones;
+	
+	public List<Operaciones> ObtenerLaListaDeOperacionesDeLaPersona()
+	{
+		List<Operaciones> laListaDeOperaciones=null;
+		
+		
+		return laListaDeOperaciones;
+	}
 
 }
